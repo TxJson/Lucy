cbf1ec81c5be1dc06771f6daceaafc537e43143c
Nylah D. Klasson <16917393+TxJson@users.noreply.github.com>
2018-09-07 21:29:03 +0200

    Small constant fix

--- Lucy/Lucy/Constants.h
+++ Lucy/Lucy/Constants.h
@@ -4,7 +4,7 @@
 #define NAME "Lucy"
 #define VERSION 0.01
 #define AUTHOR "Pontuz Klasson"
-#define LICENSE "Apache License V2.0"
+#define LICENSE "Apache License 2.0"
 #define REPOSITORY "https://github.com/Tuz1e/Lucy"
 
 #endif
